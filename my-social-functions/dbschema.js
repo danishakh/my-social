8bae3903c40208851c21af4833a8717745f64c4b
danishakh <danishakh@gmail.com>
2019-08-12 22:56:49 -0400

    added signup route and functionality

---
+++ my-social-functions/dbschema.js
@@ -0,0 +1,14 @@
+// This is a reference document for our purposes only
+// We can refer to this document to remind us of how the schema shoud look like
+
+let db = {
+    posts: [
+        {
+            username: 'user1',
+            body: 'this is the post body',
+            createdAt: '2019-08-12T22:43:33.114Z',
+            likeCount: 5,
+            commentCount: 3
+        }
+    ]
+}
--- my-social-functions/functions/index.js
+++ my-social-functions/functions/index.js
@@ -1,31 +1,59 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
+const firebase = require('firebase');
 
 const serviceAccount = require("./keys/my-social-7b8185040aea.json");
 
-// To use the admin, we need to initialize our firebase app
+
+// Initialize Express
+const app = require('express')();
+
+// To use the firebase admin, we need to initialize our firebase app
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   databaseURL: "https://my-social-ff134.firebaseio.com"
 });
 
-const app = require('express')();
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyDhJfDDDdNCHuqxF7djMygZAexBEvz0Lvs",
+    authDomain: "my-social-ff134.firebaseapp.com",
+    databaseURL: "https://my-social-ff134.firebaseio.com",
+    projectId: "my-social-ff134",
+    storageBucket: "my-social-ff134.appspot.com",
+    messagingSenderId: "929667223940",
+    appId: "1:929667223940:web:20b7c91366e6133d"
+  };
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+const db = admin.firestore();
+
+// ======================
+// API ROUTES
+// ======================
 
 
-// GET All Posts
+// GET All Posts - (/api/posts)
 app.get('/posts', (req, res) => {
-    admin
-        .firestore()
+    db  
         .collection('posts')
+        .orderBy('createdAt', 'desc')   // order our response by newest post 1st
         .get()
         .then(data => {
             let posts = [];
 
             data.forEach(doc => {
-                posts.push(doc.data()); // doc.data() - function that returns the data inside the document
+                posts.push({
+                    postId: doc.id,
+                    body: doc.data().body,
+                    username: doc.data().username,
+                    createdAt: doc.data().createdAt,
+                    commentCount: doc.data().commentCount,
+                    likeCount: doc.data().likeCount
+                }); // doc.data() - function that returns the data inside the document
             });
 
             return res.json(posts);
@@ -34,6 +62,7 @@ app.get('/posts', (req, res) => {
 })
 
 
+// Add new post - (/api/post)
 app.post('/post', (req, res) => {
     // if you send a GET method to a route which expects POST, send this error
     if(req.method !== 'POST') {
@@ -43,11 +72,10 @@ app.post('/post', (req, res) => {
     const newPost = {
         body: req.body.body,
         username: req.body.username,
-        createdAt: admin.firestore.Timestamp.fromDate(new Date())
+        createdAt: new Date().toISOString()
     }
 
-    admin
-        .firestore()
+    db
         .collection('posts')
         .add(newPost)
         .then(doc => {
@@ -56,6 +84,66 @@ app.post('/post', (req, res) => {
         .catch(err => {
             res.status(500).json({ message: 'Server Error'});
             console.error(err);
+        });
+});
+
+
+// Signup Route
+let token, userId;
+
+app.post('/signup', (req, res) => {
+    const newUser = {
+        email: req.body.email,
+        password: req.body.password,
+        confirmPassword: req.body.confirmPassword,
+        username: req.body.username
+    }
+
+    // TODO: validate data
+    db
+        .doc(`/users/${newUser.username}`)
+        .get()
+        .then(doc => {
+            // if username already exists in db, return error  
+            if(doc.exists) {
+                return res.status(400).json({ username: 'username already exists!'})
+            }
+            // register user
+            else {
+                return firebase.auth().createUserWithEmailAndPassword(newUser.email, newUser.password)
+            }
+        })
+        // If we reach here, user has been created - get token out of the created user
+        .then(data => {
+            userId = data.user.uid;
+            return data.user.getIdToken()
+        })
+        // return token as response
+        .then(idToken => {
+            token = idToken;
+            // create our user credentials to persist in our db
+            const userCredentials = {
+                username: newUser.username,
+                email: newUser.email,
+                createdAt: new Date().toISOString(),
+                userId
+            };
+
+            return db.doc(`/users/${newUser.username}`).set(userCredentials);
+        })
+        .then(() => {
+            return res.status(201).json({ token });
+        })
+        .catch(err => {
+            console.error(err);
+            // Check for client error and return 400 Bad Request
+            if (err.code === 'auth/email-already-in-use') {
+                return res.status(400).json({ email: 'Email is already in use'})
+            } 
+            // Else it's probably some Server Error
+            else {
+                return res.status(500).json({ error: err.code });
+            }
         });
 });
 
