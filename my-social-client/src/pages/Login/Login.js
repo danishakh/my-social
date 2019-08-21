4ecba52a89c6faea4054a6f80981fdfc9756164e
danishakh <danishakh@gmail.com>
2019-08-21 02:57:20 -0400

    completed login page design. need to make it responsive still for iPad level devices

--- my-social-client/src/pages/Login/Login.js
+++ my-social-client/src/pages/Login/Login.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import {Link as BrowserLink} from 'react-router-dom';
 import withStyles from '@material-ui/core/styles/withStyles';
 import PropTypes from 'prop-types';
 import appIcon from '../../images/buzzer.png';
@@ -9,6 +10,7 @@ import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
+import Link from '@material-ui/core/Link';
 import { minWidth } from '@material-ui/system';
 
 
@@ -34,15 +36,17 @@ const styles = {
         backgroundColor: 'rgb(254, 192, 56)',
         minHeight: '100%',
         minWidth: '100%',
-        position: 'relative'
+        position: 'relative',
+        paddingBottom: '5%'
     },
     image: {
         margin: '20px auto 10px auto'
-        },
+    },
     textField: {
-        margin: '10px auto 5px auto',
+        margin: '5px auto 5px auto',
         minWidth: '50%',
-        width: '70%'
+        width: '70%',
+        background: 'rgb(254, 192, 56)'
     },
     button: {
         marginTop: 20,
@@ -81,23 +85,22 @@ class Login extends Component {
 
         return (
             <Grid container className={classes.windowContainer} direction="row" justify="center" alignContent="center">
-                <Grid item sm={3} />
-                <Grid item sm={4} className={classes.paperGrid}>
+                <Grid item sm={2} md={4} />
+                <Grid item sm={8} md={4} className={classes.paperGrid}>
                         <Grid item sm>
                             <Paper elevation={20} className={classes.formPaper}>
                                 <Paper elevation={20} className={classes.logoPaper}>
                                     <img src={appIcon} alt="buzzer-bee" className={classes.image} />
                                 </Paper>
 
-                                <Typography variant="h3" className="appTitle">buzzer</Typography>
+                                <Typography style={{fontFamily: '"Long Cang", cursive'}} variant="h3" className="appTitle">buzzer</Typography>
 
-                                <form noValidate onSubmit={this.onSubmitHandler} className="loginForm">
-                                    
+                                <form noValidate onSubmit={this.onSubmitHandler} className="loginForm">  
                                     <TextField 
                                         id="email" 
                                         name="email" 
                                         type="email" 
-                                        label="Email" 
+                                        label="Email"
                                         className={classes.textField} 
                                         value={this.state.email} 
                                         onChange={this.onChangeHandler}
@@ -123,47 +126,13 @@ class Login extends Component {
                                         Login
                                     </Button>
                                 </form>
+                                <Typography style={{marginBottom: '5%'}} variant="caption" color="textSecondary">
+                                    <i>Don't have an account yet? <Link component={BrowserLink} to="/register">Register here</Link></i>
+                                </Typography>
                             </Paper>
                         </Grid>
-                    
-                    {/* <Paper elevation={20} className={classes.formPaper}>
-                        <Grid container justify="center" alignContent="center">
-                            <Grid item sm>
-                                <Paper elevation={20} className={classes.logoPaper}>
-                                    <img src={appIcon} alt="buzzer-bee" className={classes.image} />
-                                </Paper>
-                            </Grid>
-                        </Grid>
-                        
-                        <form noValidate onSubmit={this.onSubmitHandler}>
-                            <Grid className={classes.formContainer} container direction="column" justify="flex-end" alignContent="stretch">
-                                <Grid item sm={8}>
-                                <TextField 
-                                    id="email" 
-                                    name="email" 
-                                    type="email" 
-                                    label="Email" 
-                                    className={classes.textField} 
-                                    value={this.state.email} 
-                                    onChange={this.onChangeHandler}
-                                    fullWidth
-                                />
-                                </Grid>
-                                <Grid item sm={12}>
-                                <Button
-                                    type="submit"
-                                    variant="contained"
-                                    color="primary"
-                                    className={classes.button}
-                                >
-                                    Login
-                                </Button>
-                                </Grid>
-                            </Grid>
-                        </form>
-                    </Paper> */}
-                </Grid>
-                <Grid item sm={3} />
+                    </Grid>
+                <Grid item sm={2} md={4} />
             </Grid>
         )
     }
